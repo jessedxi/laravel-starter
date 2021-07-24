cd8cd7c89f8614b239229a1795047f5b875aa5fb
Jesse <jessedaniel@live.ca>
2021-07-24 15:01:09 -0600

    add title variable

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -6,8 +6,10 @@ use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
+
     public function index() {
-        return view('products.index');
+        $title = 'Welcome to my Laravel 8 practice';
+        return view('products.index', compact('title'));
     }
 
     public function about() {
